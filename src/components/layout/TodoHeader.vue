f0780514840dbfbb657d81352dffab113e4f08bb
Abdallah Bratos <bratosab@gmail.com>
2019-07-08 00:45:48 +0200

    Added header

---
+++ src/components/layout/TodoHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <header class="header">
+    <h1>Vue.js Todo List</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "TodoHeader"
+};
+</script>
+
+<style scoped>
+.header {
+  display: flex;
+  justify-content: center;
+  padding: 5px;
+  background-color: #007acc;
+  color: #ffffff;
+}
+</style>
